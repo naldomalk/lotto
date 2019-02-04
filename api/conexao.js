dde36018090b178087c057ac8fb4f74b11bfda63
Arnaldo Liberal dos Santos <naldo@MacBook-Pro-de-Arnaldo.local>
2019-02-04 11:26:50 +0100

    commit inicial

---
+++ api/conexao.js
@@ -0,0 +1,8 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'xxxx',
+  port     : XXX,
+  user     : 'XXX',
+  password : 'XXX',
+  database : 'XXX'
+});
