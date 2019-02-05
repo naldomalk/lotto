6aca8588043f53986d46b2f04c55e52255e99ff0
naldomalk <arnaldo@ahcom.com.br>
2019-02-05 09:00:10 +0100

    Update conexao.js

--- api/conexao.js
+++ api/conexao.js
@@ -2,8 +2,8 @@ const mysql      = require('mysql');
 const connection = mysql.createConnection({
   host     : 'servidor.ahcom.com.br',
   port     : 3306,
-  user     : 'ahcom',
-  password : 'vagalumes3204',
+  user     : 'xxxxxx',
+  password : 'xxxxxx',
   database : 'lotto'
 });
 
