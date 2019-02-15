a65f1cddd726c266147067e4eb5a95b6800cb0e4
Arnaldo Liberal dos Santos <naldo@MacBook-Pro-de-Arnaldo.local>
2019-02-15 02:03:03 +0100

    corrigido

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,19 +1,21 @@
 'use strict';
 
-var config = {
-    fields : 'User, Birth, Phone, Email, Password',
-    props : {
-            'Password':{type:'password',req:true},
-            1:{1:1}
-        }
+const config = {
+        fields : 'User, Birth, Phone, Email, Password',
+        props : {
+                'Password':{type:'password',req:true},
+                1:{1:1}
+            }
 }
 
 // register user
 exports.post = async(req, res, next) => {
-    var fields = config.fields;
-    var values = fn.check_post(config, req);
+    let fields = config.fields;
+    let values = fn.check_post(config, req);
 
-    var SQL = `INSERT INTO users (${fields}) VALUES (?)`;
+    console.log(fields);
+
+    let SQL = `INSERT INTO users (${fields}) VALUES (?)`;
 
     conn.query(SQL, [values], function(error, results, fields){
         if(error) 
@@ -25,21 +27,23 @@ exports.post = async(req, res, next) => {
 
 // logon user
 exports.get = async(req, res, next) => {
-    var fields = 'Email, Password'; //config.fields = fields;
+    let reconfig = Object.assign({}, config);
+    reconfig.fields = 'Email, Password'; 
 
-    var values = fn.check_post(config,req); 
+    let values = fn.check_post(reconfig,req); 
 
-    var SQL = `SELECT * 
+    let SQL = `SELECT *
                 FROM users U 
                 WHERE U.Email = ? 
                 AND U.Password = ?`;
 
-    conn.query(SQL, values, function(error, results, fields){
+    var query = conn.query(SQL, values, function(error, results, fields){
         if(error) 
         res.status(400).json(error);
         else
-        res.json(results);
+        res.send("Total: "+results.length);
     });
+    console.log(query.sql);
 }
 
 // logoff user
